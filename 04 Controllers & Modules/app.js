218e844385268195acb979fc0d8f360fb36f526f
Sameer Singh <sssaml3@gmail.com>
2015-11-20 17:57:33 +0200

    Save progress
    - Self-invoked anonymous functions are awesome: they prevent polluting
    the global namespace
    - Remember the code and syntax to deal with the minification issue on
    parameters ($inject is cool)

---
+++ 04 Controllers & Modules/app.js
@@ -0,0 +1,5 @@
+(function() {
+
+  angular.module('app', []) // Define module
+    .controllers = {}; // Custom object container for controllers
+}()); // Self-invoked anonymous function; prevents polluting global namespace
