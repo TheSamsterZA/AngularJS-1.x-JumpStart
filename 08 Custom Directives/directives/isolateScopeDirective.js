b5d7db92d6a0a5601cc142657b56f2bb83f8ddb7
Sameer Singh <sssaml3@gmail.com>
2015-12-09 15:45:48 +0200

    Section 7: Custom Directives
    - Standardize on scope properties in the directive

--- 08 Custom Directives/directives/isolateScopeDirective.js
+++ 08 Custom Directives/directives/isolateScopeDirective.js
@@ -6,24 +6,24 @@
     return { // Directive Definition Object
       restrict: 'E', // Restrict directive type to element only
       scope: { // Isolate scope
-        titleAt: '@', // One-way binding to strings only
-        versionEq: '=', // Two-way binding to strings and objects
-        actionAmp: '&' // Event delegate function
+        title: '@', // One-way binding to strings only
+        version: '=', // Two-way binding to strings and objects
+        action: '&' // Event delegate function
       },
       // Here, we are using the three different scope bindings in the template
-      // titleAt one-way binds to $scope.title:
-      // - if $scope.title vhanges, then titleAt changes
-      // - if titleAt changes, then ONLY titleAt changes. $scope.title is untouched
-      // versionEq two-way binds to $scope.Version:
-      // - if $scope.version changes, then versionEq changes, AND VICE VERSA
-      // actionAmp is a delegate to the $scope.changeTitle() function:
+      // title one-way binds to $scope.title:
+      // - if $scope.title vhanges, then title changes
+      // - if title changes, then ONLY title changes. $scope.title is untouched
+      // version two-way binds to $scope.version:
+      // - if $scope.version changes, then version changes, AND VICE VERSA
+      // action is a delegate to the $scope.changeTitle() function:
       // - fires the function, which changes $scope.title
       template: ' \
         [isolate-scope]: \
-        directiveScope.titleAt: {{ titleAt }} \
-        directiveScope.versionEq: {{ versionEq }} \
-        <button ng-click="versionEq = versionEq + 1; titleAt = \'Directive Changed Title\'">Increment Version</button> \
-        <button ng-click="actionAmp();">Run $scope.changeTitle() Function</button> \
+        directiveScope.title: {{ title }} \
+        directiveScope.version: {{ version }} \
+        <button ng-click="version = version + 1; title = \'Directive Changed Title\'">Click Me</button> \
+        <button ng-click="action();">Run $scope.changeTitle() Function</button> \
       ',
       // Below, we use the link function to do DOM manipulation
       link: function(scope, element, attributes) {
